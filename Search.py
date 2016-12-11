8bed1a3f366f9dfb3c9d6a60990dfd58d1075061
KristoKärtmann <tiskuu268@gmail.com>
2016-12-11 18:58:11 +0200

    Started on Main

--- Search.py
+++ Search.py
@@ -1,19 +1,28 @@
 import os
 import subprocess
 
-def findDirectories(exten, topdir):
+def findDirectories(extensions, topdir):
     results = str()
-
-    for dirpath, dirnames, files in os.walk(topdir):
-        for name in files:
-            if name.lower().endswith(exten):
-                results += '%s\n' % os.path.join(dirpath, name)
+    for exten in extensions:
+        for dirpath, dirnames, files in os.walk(topdir):
+            for name in files:
+                if name.lower().endswith(exten):
+                    results += '%s\n' % os.path.join(dirpath, name)
 
     results = results.split('\n')
     return results
+
 
 def click_on_file(filename):
     try:
         os.startfile(filename)
     except AttributeError:
         subprocess.call(['open', filename])
+
+def search(word, directory):
+    extensions = ['.txt','.yml', '.lua']
+    files = findDirectories(extensions,directory)
+    found = []
+    for file in files:
+        f = open(file)
+
--- UI.py
+++ UI.py
@@ -1,23 +1,48 @@
 from tkinter import *
 from Search import  *
+
 class UI:
     def __init__(self, master):
-
-        self.directoryEntry = Entry(master)
-        self.directoryEntry.grid(row=0, column=0, sticky=W)
+        master.minsize(width=666, height=666)
+        master.maxsize(width=666, height=666)
+        master.grid_columnconfigure(0, weight=1)
+        self.scrollbar = Scrollbar(master, orient=VERTICAL)
+        v = StringVar()
+        self.directoryEntry = Entry(master,textvariable=v)
+        self.directoryEntry.grid(row=0,sticky=N+S+E+W)
 
         self.directoryEntryButton = Button(master,text='OK', command=self.saveEntry)
-        self.directoryEntryButton.grid(row=0, column=1, sticky=W)
+        self.directoryEntryButton.grid(row=0,column=1,sticky=N+S+E+W)
 
-        self.list = Listbox(master)
-        self.list.grid(row=1)
+        self.list = Listbox(master,yscrollcommand=self.scrollbar.set)
+        self.scrollbar.config(command=self.list.yview)
+        self.scrollbar.grid(row=1,column=1,rowspan=2,sticky=N+S+E+W)
+        self.list.grid(row=1,rowspan=2,sticky=N+S+E+W)
+        self.list.bind('<Double-Button-1>', self.OnDouble)
+
+        mainDir = v.get()
+
+
+
+
+
+
+
 
 
     def saveEntry(self):
-        self.entry = self.directoryEntry.get()
+        self.list.delete(0,END)
+        entry = self.directoryEntry.get()
         self.directoryEntry.delete(0, END)
-        
+        lst = findDirectories(['.txt','.yml', '.lua'],entry)
+        for item in lst:
+            self.list.insert(END, item)
 
+    def OnDouble(self, event):
+        widget = event.widget
+        selection=widget.curselection()
+        value = widget.get(selection[0])
+        click_on_file(value)
 
 
 
@@ -26,5 +51,5 @@ class UI:
 
 def start():
     root = Tk()
-    test = UI(root)
+    start = AskDir(root)
     root.mainloop()
